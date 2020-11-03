135b423c4bc7403eca722ae3ebb2be3b9557796d
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-11-02 21:35:25 -0500

    update file structure

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-// This uses Heroku's process.env.PORT value when deployed and 3001 when run locally. Having a dynamic port number is important, because it is very unlikely that the port number you hardcode (e.g., 3001) would be the port Heroku runs your app on.
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
